007efbc4b14c354742e82657f84f0dda9d3c7e39
Gian Lozano <122474391+McLozanoMark@users.noreply.github.com>
2026-03-11 22:52:49 -0500

    act 11 marzo 10 52 pm

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='planner-tdah-rebuilt-v8';
+const CACHE_NAME='planner-tdah-v81';
 const APP_SHELL=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
